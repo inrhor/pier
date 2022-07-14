dc18a7f0d91ad95e8c512914eebfc4a0007d09ac
inrhor <inrhor@aliyun.com>
2022-07-14 19:15:13 +0800

    fix🐛:  进入游戏时延迟加载计分板

--- src/main/kotlin/com/github/inrhor/pier/core/listener/JoinQuitListener.kt
+++ src/main/kotlin/com/github/inrhor/pier/core/listener/JoinQuitListener.kt
@@ -5,12 +5,17 @@ import com.github.inrhor.pier.api.manager.BoardManager.reloadAuto
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common.platform.function.submit
 
 object JoinQuitListener {
 
     @SubscribeEvent
     fun join(ev: PlayerJoinEvent) {
-        ev.player.reloadAuto()
+        submit(async = true, delay = 10L) {
+            if (ev.player.isOnline) {
+                ev.player.reloadAuto()
+            }
+        }
     }
 
     @SubscribeEvent
